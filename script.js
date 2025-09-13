57051965dcfe4d9639edf4dc051eb96a359e8403
Yahaya <prop25fxt@gmail.com>
2025-09-13 13:22:22 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Contact form placeholder functionality
-document.getElementById("contact-form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("Thank you for contacting us. We will get back to you shortly!");
-});
